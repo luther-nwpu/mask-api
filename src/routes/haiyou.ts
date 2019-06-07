b312c7db949f456768d4d9397c634412f099d178
e <luthernpu@gmail.com>
2019-06-07 12:31:31 +0800

    d

--- src/routes/haiyou.ts
+++ src/routes/haiyou.ts
@@ -119,6 +119,16 @@ router.get('/getHaiyouById', async (ctx, next) => {
                 History.saveHistory(payload, haiyouId)
             }
             result.save('hot', result.get('hot') + 1)
+            console.log(await result.get('video_id').split('_').reduce(async (total, currentValue) => {
+                const accumulator = await total
+                accumulator.push(await Video.getVideo(parseInt(currentValue, 10)))
+                return Promise.resolve(accumulator)
+            }, Promise.resolve([])))
+            console.log(await (result.get('spare_picture') ? result.get('spare_picture').split('_') : []).reduce(async (total, currentValue) => {
+                const accumulator = await total
+                accumulator.push(await Picture.getPicture(parseInt(currentValue, 10)))
+                return Promise.resolve(accumulator)
+            }, Promise.resolve([])))
             resolve({videoArr: await result.get('video_id').split('_').reduce(async (total, currentValue) => {
                 const accumulator = await total
                 accumulator.push(await Video.getVideo(parseInt(currentValue, 10)))
